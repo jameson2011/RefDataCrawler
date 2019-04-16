872003fce690967818d4b49527344325f551fe97
Jameson2011 <j7db@hotmail.co.uk>
2019-04-16 01:52:46 +0100

    initial solution

---
+++ src/RefDataCrawler/CommandLine.fs
@@ -0,0 +1,51 @@
+﻿namespace RefDataCrawler
+
+module CommandLine=
+
+    open Microsoft.Extensions
+    
+    type App = CommandLineUtils.CommandLineApplication
+    type CmdOption = CommandLineUtils.CommandOption
+    type Command = App -> bool
+
+
+    
+    let setHelp(app: App) =
+        app.HelpOption("-? | -h | --help") |> ignore
+        app
+           
+    
+    let setAction (fn: Command) (app: App) =
+        let act = fun () ->     if fn(app) then 0
+                                else 2
+        app.OnExecute(act) 
+        app
+
+    let setDesc desc (app: App) =
+        app.Description <- desc
+        app
+
+
+    let createApp() =
+        let app = new App(false)
+        app.Name <- "RefDataCrawler"
+        app.Description <- "ESI reference data crawler"
+        app
+
+    let private composeAppPipe(f: App -> App) = new System.Action<App>(f >> setHelp >> ignore)
+    
+
+    let addRun cmd (app: App) =
+        let f = setDesc "Run the crawler" 
+                                    (*
+                                    >> addKillSourceUriArg
+                                    >> addMongoServerArg >> addMongoDbArg >> addMongoKillsCollectionArg >> addMongoSessionsCollectionArg
+                                    >> addMongoUserArg >> addMongoPasswordArg 
+                                    >> addWebServerPortArg
+                                    >> addLiveBufferSizeArg
+                                    >> addNoCacheArg
+                                    >> addSessionTimeoutArg
+                                    *)
+                                    >> setAction cmd
+        app.Command("run", (composeAppPipe f)) |> ignore
+        app
---
+++ src/RefDataCrawler/Program.fs
@@ -0,0 +1,48 @@
+﻿namespace RefDataCrawler
+
+open System
+
+module Program =
+
+    let private runCrawler =
+        async {
+            return 0 |> ignore
+        }
+
+    let private startCrawler (app: CommandLine.App) =        
+        let cts = new System.Threading.CancellationTokenSource()
+
+        Async.Start(runCrawler, cts.Token)
+        
+        Console.WriteLine("Hit Return to quit")
+        Console.ReadLine() |> ignore 
+
+        //"Shutting down..." |> logInfo
+
+        cts.Cancel()
+
+
+        true
+
+    let private createAppTemplate()=
+        let app = CommandLine.createApp()
+                    |> CommandLine.addRun startCrawler
+                    |> CommandLine.setHelp
+    
+        app.OnExecute(fun () -> app.ShowHelp()
+                                0)
+        app
+
+    [<EntryPoint>]
+    let main argv =
+        let app = createAppTemplate()
+    
+        app.OnExecute(fun () -> app.ShowHelp()
+                                0)
+
+        try
+                app.Execute(argv)
+
+        with
+        | ex -> ConsoleUtils.error ex.Message   
+                2
