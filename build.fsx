d472cb471c44e0cbab910e65ce9fecb671eb3427
Jameson2011 <j7db@hotmail.co.uk>
2020-07-02 22:11:21 +0100

    fix build options

--- build.fsx
+++ build.fsx
@@ -6,6 +6,21 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+let version = Fake.Core.Environment.environVarOrNone "BUILD_BUILDNUMBER" |> Option.defaultValue "0.0.0.1"
+
+let buildOptions = fun (opts: DotNet.BuildOptions) -> 
+    let props = ("AssemblyVersion", version) 
+                    :: ("AssemblyFileVersion", version) 
+                    :: opts.MSBuildParams.Properties 
+
+    { opts with
+        Configuration = DotNet.BuildConfiguration.Release
+        MSBuildParams = { opts.MSBuildParams with 
+                            DisableInternalBinLog = true
+                            Properties = props }
+        
+        }
+
 let publishOptions = (fun (opts: DotNet.PublishOptions) -> 
                           {
                             opts with 
@@ -24,7 +39,7 @@ Target.create "Clean" (fun _ ->
 
 Target.create "Build" (fun _ ->
     !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+    |> Seq.iter (DotNet.build buildOptions)
 )
 
 Target.create "Publish" (fun _ -> 
